f34d9857b361ab84ee92641ce158e3400c0c3c7d
Sam Randolph <randolph.113@osu.edu>
2013-01-09 21:18:56 -0500

    Updated window title and added gitignore

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ShipShape")]
+[assembly: AssemblyTitle("Ship Shape")]
 [assembly: AssemblyProduct("ShipShape")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Microsoft")]
